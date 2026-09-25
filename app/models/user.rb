e99cdb53d15ec57f9b61bf7b2783c81b3b7bac90
Kagari Aono <OCA2602It0001@edu.oca.ac.jp>
2026-09-25 14:39:14 +0900

    作業内容を更新

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ApplicationRecord
+  validates :name,
+            presence: true,
+            length: { minimum: 2 },
+            uniqueness: true
+
+  validates :age,
+            presence: true,
+            numericality: { only_integer: true }
+
+  validates :password,
+            presence: true,
+            length: { in: 6..20 },
+            confirmation: true
+
+  validates :password_confirmation,
+            presence: true
+
+  validates :email,
+            format: {
+              with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
+              message: "使用できない文字が含まれているか形式に誤りがあります。"
+            },
+            uniqueness: true
+
+   devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_28_061505) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_25_045616) do
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "description"
     t.string "name", null: false
     t.integer "price", null: false
     t.string "string"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.string "password"
     t.datetime "updated_at", null: false
   end
 end
